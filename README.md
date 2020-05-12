37d34aac0b974354c8839a7481899a3fb9dd05e5
Vance <vancejarwell@gmail.com>
2020-05-12 14:47:50 +0800

    fist commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# VanceJarwell.github.io
--- footer.php
+++
@@ -1,6 +0,0 @@
-<div id="footer">
-  <div class="container">
-  	<p class="text-muted">Today is <?php echo date("y-m-d");?>. &nbsp;&nbsp;&copy; Xenia Qian, June 2014. </p>
-
-  </div>
-</div>
